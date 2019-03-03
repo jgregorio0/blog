62a92af7130d07f963396644b2271eff4f6647ce
jgregorio <gregoriojesus0@gmail.com>
2019-03-03 21:40:39 +0100

    Fix title

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,23 +7,26 @@
 </template>
 
 <script>
-  import TheHeader from '@/components/Navigation/TheHeader.vue'
-  import TheSidenav from '@/components/Navigation/TheSidenav.vue'
+import TheHeader from '@/components/Navigation/TheHeader.vue'
+import TheSidenav from '@/components/Navigation/TheSidenav.vue'
 
-  export default {
-    components: {
-      TheHeader,
-      TheSidenav
-    }
+export default {
+  head: {
+    title: 'Blog - Jesús Gregorio Pérez - jgregorio'
+  },
+  components: {
+    TheHeader,
+    TheSidenav
   }
+}
 </script>
 
 <style>
-  html {
-    font-family: 'Open Sans', sans-serif;
-  }
+html {
+  font-family: "Open Sans", sans-serif;
+}
 
-  body {
-    margin: 0;
-  }
+body {
+  margin: 0;
+}
 </style>
