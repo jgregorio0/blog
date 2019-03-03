2d17ac5ad342060dda2fe140d5cade30b86647d4
jgregorio <gregoriojesus0@gmail.com>
2019-03-03 21:44:07 +0100

    Show index only in main list post as preview

--- components/Posts/PostPreview.vue
+++ components/Posts/PostPreview.vue
@@ -1,36 +1,45 @@
 <template>
-  <b-card class="post-preview" 
-    :footer="updatedDate">
-    <div slot="header" >
+  <b-card class="post-preview" :footer="updatedDate" :id="this.id">
+    <div slot="header">
       <!-- TITLE / DETAIL -->
-      <nuxt-link class="btn btn-link" tag="button" title="Go to detail"  
-          :to="'/posts/' + id">
+      <nuxt-link
+        class="btn btn-link post-title"
+        tag="button"
+        title="Go to detail"
+        :to="'/posts/' + id"
+      >
+        <fa :icon="faExternalLinkAlt"/>
         <span>{{title}}</span>
-        <fa :icon="faExternalLinkAlt" />
       </nuxt-link>
-       <!-- DOWNLOAD -->
+      <!-- DOWNLOAD -->
       <b-button-group class="float-right">
-        
         <b-button @click="downloadFile" variant="link">
-          <fa :icon="faDownload" />
+          <fa :icon="faDownload"/>
         </b-button>
       </b-button-group>
     </div>
-    <p class="card-text post-content" v-html="contentHTML"></p>
+    <p
+      v-cloak
+      v-show="false"
+      :class="'card-text post-content js-toc-content-' + this.id"
+      v-html="contentHTML"
+    ></p>
+    <p :class="'js-toc-' + this.id"></p>
   </b-card>
 </template>
 
 <script>
-import showdown from "showdown";
-import tocbot from "tocbot";
+import showdown from 'showdown'
+// eslint-disable-next-line semi
+import tocbot from 'tocbot';
 import {
   faExternalLinkAlt,
   faDownload
-} from "@fortawesome/free-solid-svg-icons";
-import { download } from "~/services/download.js";
+} from '@fortawesome/free-solid-svg-icons'
+import { download } from '~/services/download.js'
 
 export default {
-  name: "PostPreview",
+  name: 'PostPreview',
   props: {
     id: {
       type: String,
@@ -50,28 +59,67 @@ export default {
     }
   },
   computed: {
-    contentHTML() {
-      let converter = new showdown.Converter();
-      return converter.makeHtml(this.content);
+    contentHTML () {
+      let converter = new showdown.Converter()
+      return converter.makeHtml(this.content)
     },
-    faExternalLinkAlt() {
-      return faExternalLinkAlt;
+    toc () {
+      this.contentHTML
+      // eslint-disable-next-line no-undef
+      return toc.refresh()
     },
-    faDownload() {
-      return faDownload;
+    faExternalLinkAlt () {
+      return faExternalLinkAlt
+    },
+    faDownload () {
+      return faDownload
     }
   },
   methods: {
-    downloadFile() {
-      download(this.title + ".md", this.content);
+    downloadFile () {
+      download(this.title + '.md', this.content)
     }
+  },
+  mounted () {
+    tocbot.init({
+      // Where to render the table of contents.
+      tocSelector: '.js-toc-' + this.id,
+      // Where to grab the headings to build the table of contents.
+      contentSelector: '.js-toc-content-' + this.id,
+      // Which headings to grab inside of the contentSelector element.
+      headingSelector: 'h1, h2, h3',
+      // overwrite link href behavior for anchors
+      onClick: function (e) {
+        e.preventDefault()
+
+        var elem = this
+        for (
+          ;
+          elem && elem !== document;
+          elem = elem.parentNode
+        ) {
+          if (elem.className && elem.className.includes('post-preview')) {
+            location.href =
+              'posts/' +
+              elem.id +
+              this.href.substring(this.href.indexOf('#'), this.href.length)
+          }
+        }
+      }
+    })
   }
-};
+}
 </script>
 
 <style scoped>
 .post-content {
   height: 200px;
   overflow-y: scroll;
+}
+.post-title {
+  width: 240px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  text-align: left;
 }
 </style>
