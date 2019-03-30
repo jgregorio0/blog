09053d5c440d0ddac4366abfa9d315457a87e520
jgregorio <gregoriojesus0@gmail.com>
2019-03-31 00:17:06 +0100

    adapt detail view as theme

--- components/Posts/PostDetail.vue
+++ components/Posts/PostDetail.vue
@@ -1,23 +1,16 @@
 <template>
-  <div class="single-post-page">
-    <section class="post container">
-      <article>
-        <!-- TITLE AND DETAILS -->
-        <h1 class="post-title">{{ loadedPost.title }}</h1>
-        <div class="post-details">
-          <div class="post-detail">Last updated on {{ loadedPost.updatedDate }}</div>
+  <article>
+    <div class="single-post-page mt-5 container">
+      <!-- MARKDOWN VIEWER -->
+      <div class="row">
+        <div class="col-12 order-2 col-md-9 order-md-1 post-content js-toc-content">
+          <!-- TODO MD VIEWER -->
+          <div v-html="contentHTML"></div>
         </div>
-        <!-- MARKDOWN VIEWER -->
-        <div class="row">
-          <div class="col-12 order-2 col-md-9 order-md-1 post-content js-toc-content">
-            <!-- TODO MD VIEWER -->
-            <span v-html="contentHTML"></span>
-          </div>
-          <div class="col-12 order-1 col-md-3 order-md-2 js-toc"></div>
-        </div>
-      </article>
-    </section>
-  </div>
+        <div class="col-12 order-1 col-md-3 order-md-2 js-toc"></div>
+      </div>
+    </div>
+  </article>
 </template>
 
 <script>
@@ -25,12 +18,12 @@
 import showdown from "showdown";
 
 export default {
-  name: 'postDetail',
+  name: "postDetail",
   props: {
     loadedPost: {
       type: Object,
       required: true
-    },
+    }
   },
   computed: {
     contentHTML() {
@@ -52,10 +45,13 @@ export default {
       headingSelector: "h1, h2, h3",
       // overwrite link href behavior for anchors
       onClick: function(e) {
-        e.preventDefault()
-        location.hash=this.href.substring(this.href.indexOf("#"), this.href.length)
+        e.preventDefault();
+        location.hash = this.href.substring(
+          this.href.indexOf("#"),
+          this.href.length
+        );
       }
-    })
+    });
   }
-}
+};
 </script>
