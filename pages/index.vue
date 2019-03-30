f08dba963ec884e73a11bef8a7c6d7cb64387fa3
jgregorio <gregoriojesus0@gmail.com>
2019-03-31 00:20:16 +0100

    add pagination to home list

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,28 @@
 <template>
-  <div class="home-page">
-    <!-- <h1>Latest Posts</h1> -->
+  <div class="home-page container">
+    <Pager></Pager>
     <PostList :posts="loadedPosts"></PostList>
+    <Pager></Pager>
   </div>
 </template>
 
 <script>
+
+import Pager from "@/components/Navigation/Pager.vue";
+
 export default {
+  components: {
+    Pager
+  },
   computed: {
     loadedPosts() {
-      return this.$store.getters.loadedPosts;
+      const posts = this.$store.getters.loadedPosts
+      const page = this.page
+      return posts.length > 9 ? posts.slice(9 * (page - 1), 9 * page): posts
+    },
+    page () {
+      const page = this.$store.getters.page
+      return page
     }
   }
 };
