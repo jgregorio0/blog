39da897c3b8d90c0293379d2568b5cf8ad5ffe5c
jgregorio <gregoriojesus0@gmail.com>
2019-03-31 00:16:41 +0100

    Create header and layout for detail

---
+++ components/Navigation/HeaderDetail.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="masthead" style="background-image: url('/img/enjoyment.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="site-heading">
+            <h1>{{ loadedPost.title }}</h1>
+            <span class="subheading">Last updated on {{ loadedPost.updatedDate }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+<script>
+export default {
+  name: "headerDetail",
+  props: {
+    loadedPost: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
---
+++ layouts/detail.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Navigation></Navigation>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+import Navigation from '@/components/Navigation/Navigation.vue'
+
+export default {
+  head: {
+    title: 'Blog - Jesús Gregorio Pérez - jgregorio'
+  },
+  components: {
+    Navigation
+  }
+}
+</script>
+
+<style>
+html {
+  font-family: "Open Sans", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
