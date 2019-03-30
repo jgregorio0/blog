cfdaea17f53b6ece36d13abfefc011b82d236197
jgregorio <gregoriojesus0@gmail.com>
2019-03-31 00:18:56 +0100

    separate in 1, 2 or 3 columns cards depending on screen size

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -1,14 +1,16 @@
 <template>
   <section class="post-list">
     <b-card-group columns>
-      <component :is='preview'
+      <component
+        :is="preview"
         v-for="post in posts"
         :key="post.id"
         :id="post.id"
         :title="post.title"
         :content="post.content"
         :updatedDate="post.updatedDate"
-        @rmPost="rmPost"></component>
+        @rmPost="rmPost"
+      ></component>
     </b-card-group>
   </section>
 </template>
@@ -27,9 +29,9 @@ export default {
       type: Array,
       required: true
     },
-    preview:{
+    preview: {
       type: String,
-      default: 'PostPreview'
+      default: "PostPreview"
     }
   },
   methods: {
@@ -39,3 +41,20 @@ export default {
   }
 };
 </script>
+
+<style>
+@media (max-width: 992px) {
+  .card-columns {
+    -webkit-column-count: 2;
+    -moz-column-count: 2;
+    column-count: 2;
+  }
+}
+@media (max-width: 768px) {
+  .card-columns {
+    -webkit-column-count: 1;
+    -moz-column-count: 1;
+    column-count: 1;
+  }
+}
+</style>
